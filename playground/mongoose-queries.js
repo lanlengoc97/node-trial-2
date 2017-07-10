aba8074c0ec2e402b6aa7d3e1b4a10df68747ae6
lanlengoc97 <Orchid.MagnoliaLe@gmail.com>
2017-07-11 00:56:11 +0700

    queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,28 @@
+const {ObjectID}= require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/model/todo');
+
+var id ='5aa95d0c09e852a223e680ad36';
+
+if(!ObjectID.isValid(id)) {
+	console.log('ID not valid');
+}
+
+Todo.find({
+	_id:id
+}).then((todos)=> {
+	console.log('Todos',todos);
+})
+
+Todo.findOne({
+	_id:id
+}).then((todo)=> {
+	console.log('Todo',todo);
+})
+
+Todo.findById(id).then((todo)=> {
+	if(!todo) {
+		return console.log("Id not found");
+	}
+	console.log(todo);
+}).catch((e)=>console.log(e));
